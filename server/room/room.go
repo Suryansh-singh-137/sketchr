ce287aa7a47fd4d8484c7cc0262eefc7cb66b526
Surya <suryanshsingh9736@gmail.com>
2026-03-22 15:13:50 +0530

    func which intialise  a new room  for every match

---
+++ server/room/room.go
@@ -0,0 +1,41 @@
+package room
+
+import (
+	"math/rand"
+
+	"github.com/Suryansh-singh-137/sketchr-server/hub"
+)
+
+type Room struct {
+	ID           string     `json:"id"`
+	Clients      map[*hub.Client]bool `json:"clients"`
+	Broadcast    chan []byte `json:"broadcast"`
+	Register     chan *hub.Client  `json:"register"`
+	Unregister   chan *hub.Client `json:"unregister"`
+	MaxPlayers   int `json:"maxplayer"` 
+	IsGameActive bool `json:"isgameactive"` 
+}
+// genrate a uniue  room code of 6 charcter 
+
+func generateRoomCode()string {
+ chars := []string {"aus","tra","vis","head","137","travis","gng","cwc"}
+ code :="" 
+
+for i := 0; i <= 1; i++ {
+    code += chars[rand.Intn(len(chars))]
+}
+ 
+ return  code
+}
+//  new room is getting created with fresh id  ,maxplayer  , and channel intialising for tht particular room 
+func NewRoom() *Room{
+ return   &Room{
+	ID:  generateRoomCode(),
+	Clients:  make(map[*hub.Client]bool),
+	Broadcast:  make(chan []byte),
+	Register:  make(chan *hub.Client),
+	Unregister: make(chan *hub.Client),
+	MaxPlayers:  8,
+	IsGameActive:  false,
+ }
+}
